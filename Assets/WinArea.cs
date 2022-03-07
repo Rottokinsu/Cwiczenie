adcc51542d95c9f4a64829150450415e72b96018
Florian Hawryszuk <lijumi221@gmail.com>
2022-03-07 18:22:38 +0100

    halleluja
    alleluja

---
+++ Assets/WinArea.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinArea : MonoBehaviour
+{
+    public string newLvL;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLvL);
+        }
+
+    }
+}
